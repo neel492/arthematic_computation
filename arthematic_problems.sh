e17c92950b2c6ae57ff072d1ac51ff55b07ad600
neelu <neelimatalluri98@gmail.com>
2022-01-07 20:39:40 +0530

    uc1 completed

---
+++ arthematic_problems.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+read -p "Enter First numbers: " a
+
+read -p "Enter Second numbers: " b
+read -p "Enter Third numbers: " c
+echo "First number is: "$a
+
+echo "Second number is: "$b
+echo "Third number is: "$c

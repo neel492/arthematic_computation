8a3f29b65fe713feaad9fbd996106d932c2feb53
neelu <neelimatalluri98@gmail.com>
2022-01-07 21:34:57 +0530

    5uc complete

--- arthematic_problems.sh
+++ arthematic_problems.sh
@@ -1,14 +1,30 @@
-
-
 #! /bin/bash
+ 2
+ 3 declare -A oprn
+ 4
+ 5 read -p "Enter First number: " a
+ 6 read -p "Enter Second number: " b
+ 7 read -p "Enter Third number: " c
+ 8
+ 9 oprn[key1]=$((a*b+c))
+10 oprn[key2]=$((c+a/b))
+11 oprn[key3]=$((a%b+c))
+12
+13 echo "the result of First oprn is: "${oprn[key1]}
+14 echo "the result of second oprn is: "${oprn[key2]}
+15 echo "the result of third oprn is: "${oprn[key3]}
 
-read -p "Enter First numbers: " a
 
-read -p "Enter Second numbers: " b
-read -p "Enter Third numbers: " c
-echo "First number is: "$a
 
-echo "Second number is: "$b
-echo "Third number is: "$c
-z=$((a%b+c))
-echo  "print result" $z
+
+
+
+
+
+
+
+
+
+
+
+

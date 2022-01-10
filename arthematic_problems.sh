b09a0021c27b2f12154ea9959106f347dceb870e
neelu <neelimatalluri98@gmail.com>
2022-01-10 13:42:25 +0530

    uc8

--- arthematic_problems.sh
+++ arthematic_problems.sh
@@ -1,30 +1,17 @@
 #! /bin/bash
- 2
- 3 declare -A oprn
- 4
- 5 read -p "Enter First number: " a
- 6 read -p "Enter Second number: " b
- 7 read -p "Enter Third number: " c
- 8
- 9 oprn[key1]=$((a*b+c))
-10 oprn[key2]=$((c+a/b))
-11 oprn[key3]=$((a%b+c))
-12
-13 echo "the result of First oprn is: "${oprn[key1]}
-14 echo "the result of second oprn is: "${oprn[key2]}
-15 echo "the result of third oprn is: "${oprn[key3]}
 
+declare -A oprn
 
+read -p "Enter First number: " a
+read -p "Enter Second number: " b
+read -p "Enter Third number: " c
 
+oprn[0]=$((a*b+c))
+oprn[1]=$((c+a/b))
+oprn[2]=$((a%b+c))
 
+echo "the result of First oprn is: "${oprn[0]}
+echo "the result of second oprn is: "${oprn[1]}
+echo "the result of third oprn is: "${oprn[2]}
 
-
-
-
-
-
-
-
-
-
-
+echo "Result in the Descending Order: "${oprn[@]} | sort
